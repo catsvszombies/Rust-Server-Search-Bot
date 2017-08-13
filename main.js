9a50b794ee27bb4bf342ae3d9edd531b781f28e9
catsvszombies <catsvszombies@gmail.com>
2017-08-13 16:50:54 -0400

    use result page size of 100 (max) and sort by last wipe

--- main.js
+++ main.js
@@ -1,64 +1,62 @@
 console.log("main.js");
-var mainButton=document.getElementById("button");
-
-var godInfo;
 
 var token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImJlMTcyYWJhYWYyMzEzYjciLCJpYXQiOjE1MDI1OTQ2NDIsIm5iZiI6MTUwMjU5NDY0MiwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjoyNjI";
+var bmTestLink = "https://api.battlemetrics.com/servers";
+var result;
 
+var mainButton = document.getElementById("button");
 mainButton.addEventListener("click", buttonFunction);
 
-
-function buttonFunction(){
+function buttonFunction() {
   console.log("buttonFunction()");
   httpGet(bmTestLink);
 }
 
-var bmTestLink = "https://api.battlemetrics.com/servers";
-
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
       if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     }
-    var params = "filter[game]=rust";
+    var params = "filter[game]=rust&page[size]=100&sort=distance";
     xmlHttp.open("GET", theUrl+"?"+params, true); // true for asynchronous
     xmlHttp.setRequestHeader("Authorization", token);
 
     xmlHttp.send(null);
 }
 
-
-
 function callback(responseText) {
   console.log("callback");
   console.log(responseText);
 
-  godInfo = JSON.parse(responseText);
-  var formatted = JSON.stringify(godInfo, null, 2);
+  result = JSON.parse(responseText);
+  var data = result.data;
 
+  var formatted = JSON.stringify(result, null, 2);
   document.getElementById("data").innerText = formatted;
 
-  var buffer = "";
-  for (var i=0; i<godInfo.data.length; i++) {
-    buffer += godInfo.data[i].attributes.name;
-    if (godInfo.data[i].attributes.details) {
-      buffer += " - last wipe: " + godInfo.data[i].attributes.details.rust_last_wipe;
+  data.sort(function(a, b) {
+    if (!(a.attributes.details && a.attributes.details.rust_last_wipe)) {
+      return 1;
     }
-    buffer += "\n";
+    if (!(b.attributes.details && b.attributes.details.rust_last_wipe)) {
+      return -1;
+    }
+    return new Date(b.attributes.details.rust_last_wipe) - new Date(a.attributes.details.rust_last_wipe);
+  });
+
+  var buffer = "";
+  for (var i=0; i<data.length; i++) {
+    buffer += "<tr>";
+    buffer += "<td class=\"server-name\">" + data[i].attributes.name + "</td>";
+    buffer += "<td class=\"last-wipe\">";
+    if (data[i].attributes.details && data[i].attributes.details.rust_last_wipe) {
+      var lastWipe = new Date(data[i].attributes.details.rust_last_wipe);
+      buffer += lastWipe;
+    }
+    buffer += "</td>";
+    buffer += "</tr>\n";
   }
 
-  document.getElementById("content").innerText = buffer;
-
-
-  //console.log("Callback: " + responseText);
-  /*console.log("godInfo: " + godInfo);
-  for(var i = 0; i < godInfo.size; i++){
-    console.log("Element #" + i + ": " + godInfo[i]);
-  }*/
+  document.getElementById("servers").innerHTML = buffer;
 }
-
-
-
-//Time to use godInfo
